09062549e6ef7e436803ced0f7900380e30f164c
William Bonvini <w.bonvini@reply.it>
2023-05-22 23:19:55 +0200

    add spinner

--- shawn/utils.py
+++ shawn/utils.py
@@ -1,4 +1,11 @@
 import openai
+from rich.markdown import Markdown
+from rich.panel import Panel
+from rich.console import Console
+
+from shawn import DETAILS
+
+console = Console()
 
 
 def get_response(content: str) -> str:
@@ -12,3 +19,14 @@ def get_response(content: str) -> str:
     return response
 
 
+def explain_file(p: str) -> None:
+    content = f"Could you please clarify the function of the following code? Begin the explanation with an h1 markdown header that contaisn the name of the file ({p}). Follow it by your explanation. Show code snippets if you think it's necessary. \n\n"
+    content += DETAILS
+    with open(p, "r") as f:
+        content += f.read()
+
+    response = get_response(content)
+
+    md = Markdown(response)
+    panel = Panel(md, title="Shawn", border_style="blue")
+    console.print(panel)
