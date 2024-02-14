137a9c310f7d32fa5ff34a5114086edaa6bf815c
William Bonvini <w.bonvini@reply.it>
2024-02-14 21:35:28 +0100

    inject assistant to utils class

--- shawn/__init__.py
+++ shawn/__init__.py
@@ -3,7 +3,6 @@ from importlib.metadata import PackageNotFoundError, version
 
 from halo import Halo
 
-from gptassistant import GPTAssistant
 
 try:
     __version__ = version("shawn")
@@ -16,4 +15,3 @@ del version
 api_key = os.getenv("OPENAI_API_KEY")
 
 spinner = Halo(text="Thinking...", spinner="flip")
-assistant = GPTAssistant(api_key=api_key)
